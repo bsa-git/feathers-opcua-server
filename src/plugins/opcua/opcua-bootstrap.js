ecd199f5ea8f2941e51485d069f8b5d0215a0382
bsa-git <bsa2657@yandex.ru>
2021-02-19 14:08:16 +0200

    Update

--- src/plugins/opcua/opcua-bootstrap.js
+++ src/plugins/opcua/opcua-bootstrap.js
@@ -57,24 +57,6 @@ module.exports = async function opcuaBootstrap(app) {
       if (isLog) inspector('opcuaBootstrap.opcuaClient:', opcuaClient.client.getClientInfo());
       // Execute client script
       await executeOpcuaClientScript(service, option.id);
-
-      // await service.subscriptionCreate(id);
-
-      // const srvCurrentState = await service.getSrvCurrentState(id);
-      // // Start subscriptionMonitor
-      // let variables = srvCurrentState.paramsAddressSpace.variables;
-      // variables = variables.filter(v => v.ownerGroup === 'CH_M51::ValueFromFile').map(v => v.browseName);
-      // const groups = getGroupsFromArray(variables, 10);
-      // for (let index = 0; index < groups.length; index++) {
-      //   const group = groups[index];
-      //   const nodeIds = await service.getNodeIds(id, group);
-      //   for (let index2 = 0; index2 < nodeIds.length; index2++) {
-      //     const nodeId = nodeIds[index2];
-      //     await service.subscriptionMonitor(id, 'onChangedCH_M5Handler', { nodeId });
-      //   }
-      // }
-
-      // inspector('opcuaBootstrap.opcuaClient:', opcuaClient.client.getClientInfo());
     }
   }
 };
