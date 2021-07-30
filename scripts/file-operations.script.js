0af1a14b5a28737be7f144b366311de6e9b7a23b
bsa-git <bsa2657@yandex.ru>
2021-07-30 09:17:36 +0300

    Update

--- scripts/file-operations.script.js
+++ scripts/file-operations.script.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 const assert = require('assert');
-const { 
-  appRoot, 
+const {
+  appRoot,
   inspector,
   strReplace,
   getFloat
@@ -24,11 +24,15 @@ const isLog = true;
 
 // Script Execution
 const scriptExecution = {
+  // Update AddressSpaceOptions.json
   updateAddressSpaceOptions: false,
+  // Converter from `Fox` excel data `.csv` file to KEPServer
   converterCsvForKEPServer: false,
+  // Converter from `Fox` hist data `.inp` file to KEPServer
   converterInpForKEPServer: true
 };
 
+// Config for converter from `Fox` excel data `.csv` file to KEPServer
 const csvKepServerConfig = {
   fields: {
     column1: { fromField: 'TAG', toField: 'Tag Name', default: undefined },
@@ -52,16 +56,17 @@ const csvKepServerConfig = {
   filter: ['AI', 'AIs', 'AO'],
   delimiterFrom: ';',
   delimiterTo: ',',
-  path: 'scripts/api/KEPServer',
-  jsonFileName: 'm52_v210616_1.json',
-  csvFileNameFrom: 'm52_v210616_1.csv',
-  csvFileNameTo: 'm52-kepServer_v210616_1.csv',
+  path: 'scripts/api/toKEPServer/fromFoxExcel',
+  jsonFileName: 'm52_v210616.json',
+  csvFileNameFrom: 'm52_v210616.csv',
+  csvFileNameTo: 'm52-kepServer_v210616.csv',
 };
 
+// Config for converter from `Fox` hist data `.inp` file to KEPServer
 const inpKepServerConfig = {
   fields: {
-    column1: { fromField: 'TAG', toField: 'Tag Name', default: undefined },
-    column2: { fromField: ['Compnd', 'TAG'], toField: 'Address', default: undefined },
+    column1: { fromField: 'NAMEINCOL', toField: 'Tag Name', default: undefined },
+    column2: { fromField: ['NAMEINCOL'], toField: 'Address', default: undefined },
     column3: { fromField: '', toField: 'Data Type', default: 'Default' },
     column4: { fromField: '', toField: 'Respect Data Type', default: 1 },
     column5: { fromField: '', toField: 'Client Access', default: 'RO' },
@@ -69,29 +74,30 @@ const inpKepServerConfig = {
     column7: { fromField: '', toField: 'Scaling', default: undefined },
     column8: { fromField: '', toField: 'Raw Low', default: undefined },
     column9: { fromField: '', toField: 'Raw High', default: undefined },
-    column10: { fromField: 'Lsco1', toField: 'Scaled Low', default: undefined },
-    column11: { fromField: 'Hsco1', toField: 'Scaled High', default: undefined },
+    column10: { fromField: 'LORANGE', toField: 'Scaled Low', default: undefined },
+    column11: { fromField: 'HIRANGE', toField: 'Scaled High', default: undefined },
     column12: { fromField: '', toField: 'Scaled Data Type', default: undefined },
     column13: { fromField: '', toField: 'Clamp Low', default: undefined },
     column14: { fromField: '', toField: 'Clamp High', default: undefined },
-    column15: { fromField: 'Eng', toField: 'Eng Units', default: undefined },
-    column16: { fromField: ['Descr1', 'Descr2'], toField: 'Description', default: undefined },
+    column15: { fromField: '', toField: 'Eng Units', default: undefined },
+    column16: { fromField: '', toField: 'Description', default: undefined },
     column17: { fromField: '', toField: 'Negate Value', default: undefined },
   },
-  filter: ['AI', 'AIs', 'AO'],
+  // filter: e.g. ['PNT', 'MEAS', 'SPT', 'OUT'],
+  filter: ['PNT'],
   inpFilter: ['NAMEINCOL', 'HIRANGE', 'LORANGE'],
-  delimiterFrom: ';',
   delimiterTo: ',',
-  path: 'scripts/api/KEPServer',
-  jsonFileName: 'm51_v210729_1.json',
+  path: 'scripts/api/toKEPServer/fromFoxHist',
+  jsonFileName: 'm51_v210729.json',
   inpFileNameFrom: '51hs00_v210729.inp',
-  csvFileNameTo: 'm51-kepServer_v210729_1.csv',
+  csvFileNameTo: 'm51-kepServer_v210729.csv',
 };
 
 describe('<<=== ScriptOperations: (file-operations.script) ===>>', () => {
 
+  // Update AddressSpaceOptions.json
   if (scriptExecution.updateAddressSpaceOptions) {
-    it('#1: FileOperations: update AddressSpaceOptions.json', () => {
+    it('#1: ScriptOperations: update AddressSpaceOptions.json', () => {
       let path = 'src/api/opcua/ua-cherkassy-azot-m5/AddressSpaceOptions.json';
       let options = readFileSync([appRoot, path]);
       options = JSON.parse(options);
@@ -107,6 +113,7 @@ describe('<<=== ScriptOperations: (file-operations.script) ===>>', () => {
     });
   }
 
+  // Converter from Fox Excel data `.csv` file to KEPServer
   if (scriptExecution.converterCsvForKEPServer) {
     it('#2: ScriptOperations: Converter csv files for KEPServer', () => {
       try {
@@ -164,6 +171,7 @@ describe('<<=== ScriptOperations: (file-operations.script) ===>>', () => {
     });
   }
 
+  // Converter from Fox hist data `.inp` file to KEPServer
   if (scriptExecution.converterInpForKEPServer) {
     it('#3: ScriptOperations: Converter inp files for KEPServer', () => {
       let data, re, points = [], point;
@@ -172,7 +180,7 @@ describe('<<=== ScriptOperations: (file-operations.script) ===>>', () => {
       let path = config.path;
       let fileName = config.jsonFileName;
       // Remove json file
-      if(doesFileExist([appRoot, path, fileName])) {
+      if (doesFileExist([appRoot, path, fileName])) {
         removeFileSync([appRoot, path, fileName]);
       }
       //--- convert inp file to KEPServer ---
@@ -185,7 +193,7 @@ describe('<<=== ScriptOperations: (file-operations.script) ===>>', () => {
       // Get array items
       points = data.split(re);
       // Delete points[0] item
-      points.shift(); 
+      points.shift();
       // Remove unnecessary characters from the last element of the array      
       const lastIndex = points.length - 1;
       const lastItem = points[lastIndex];
@@ -222,16 +230,50 @@ describe('<<=== ScriptOperations: (file-operations.script) ===>>', () => {
         points[index] = point;
       }
 
-      for (let index = 0; index < points.length; index++) {
-        const item = points[index];
-        inspector(`points.item_${index + 1}:`, item);
-      }
-      debug('points.length:', points.length);
-      
+      // Filter points
+      points = points.filter(item => {
+        const filterItem = item.NAMEINCOL.split('.')[1];
+        return config.filter.includes(filterItem);
+      });
+
+      // Show points
+      if (isLog) inspector('points.items:', points);
+      // Show points length
+      if(isDebug) debug('points.length:', points.length);
+
       // Write data to json file
       fileName = config.jsonFileName;
       writeJsonFileSync([appRoot, path, fileName], points);
-      
+
+      // Convert data
+      const fields = config.fields;
+      points = points.map(item => {
+        let result = {};
+        result[fields.column1.toField] = item[fields.column1.fromField].split('.')[0].split(':')[1];
+        result[fields.column2.toField] = `52AW00/${item[fields.column2.fromField]}\\hist00`;
+        result[fields.column3.toField] = item[fields.column3.default];
+        result[fields.column4.toField] = item[fields.column4.default];
+        result[fields.column5.toField] = item[fields.column5.default];
+        result[fields.column6.toField] = item[fields.column6.default];
+        result[fields.column7.toField] = item[fields.column7.default];
+        result[fields.column8.toField] = item[fields.column8.default];
+        result[fields.column9.toField] = item[fields.column9.default];
+        result[fields.column10.toField] = item[fields.column10.fromField];
+        result[fields.column11.toField] = item[fields.column11.fromField];
+        result[fields.column12.toField] = item[fields.column12.default];
+        result[fields.column13.toField] = item[fields.column13.default];
+        result[fields.column14.toField] = item[fields.column14.default];
+        result[fields.column15.toField] = item[fields.column15.fromField];
+        result[fields.column16.toField] = item[fields.column16.fromField];
+        result[fields.column17.toField] = item[fields.column17.default];
+        return result;
+      });
+
+      // Write data to csv file
+      let csv = papa.unparse(points, { delimiter: config.delimiterTo });
+      fileName = config.csvFileNameTo;
+      writeFileSync([appRoot, path, fileName], csv);
+
       assert.ok(true, 'update AddressSpaceOptions.json');
     });
   }
