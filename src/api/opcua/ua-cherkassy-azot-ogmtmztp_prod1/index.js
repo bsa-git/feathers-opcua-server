014ffdb1e10175b64607d05e63898a3eedab2b6c
bsa-git <bsa2657@yandex.ru>
2021-10-28 14:40:12 +0300

    Update

--- src/api/opcua/ua-cherkassy-azot-ogmtmztp_prod1/index.js
+++ src/api/opcua/ua-cherkassy-azot-ogmtmztp_prod1/index.js
@@ -1,8 +1,19 @@
-const loMerge = require('lodash/merge');
+const loMergeWith = require('lodash/mergeWith');
 const CH_A3_Options = require('./CH-A3_Options.json');
 const CH_M2_Options = require('./CH-M2_Options.json');
 const CH_M7_Options = require('./CH-M7_Options.json');
 const CH_M9_Options = require('./CH-M9_Options.json');
 const CH_NG_UPG2_Options = require('./CH-NG-UPG2_Options.json');
 
-module.exports = loMerge({}, CH_A3_Options, CH_M2_Options, CH_M7_Options, CH_M9_Options, CH_NG_UPG2_Options);
+function customizer(objValue, srcValue) {
+  if (Array.isArray(objValue)) {
+    return objValue.concat(srcValue);
+  }
+}
+
+let mergeResult = loMergeWith(CH_A3_Options, CH_M2_Options, customizer);
+mergeResult = loMergeWith(mergeResult, CH_M7_Options, customizer);
+mergeResult = loMergeWith(mergeResult, CH_M9_Options, customizer);
+mergeResult = loMergeWith(mergeResult, CH_NG_UPG2_Options, customizer);
+
+module.exports = mergeResult;
