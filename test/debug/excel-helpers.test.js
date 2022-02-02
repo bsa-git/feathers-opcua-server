b7c303f9a1e5e63b059051e5328f3c8428cb3309
bsa-git <bsa2657@yandex.ru>
2022-02-02 16:10:40 +0200

    Update

--- test/debug/excel-helpers.test.js
+++ test/debug/excel-helpers.test.js
@@ -9,11 +9,16 @@ const {
   inspector,
   startListenPort, 
   stopListenPort,
+  makeDirSync,
+  removeFilesFromDirSync,
+  getFileName
 } = require('../../src/plugins');
 
 const {
   xlsxGetCellsFromFile,
-  exeljsGetCellsFromFile 
+  xlsxWriteToFile,
+  exeljsGetCellsFromFile, 
+  exeljsWriteToFile
 } = require('../../src/plugins/excel-helpers');
 
 const chalk = require('chalk');
@@ -30,10 +35,12 @@ describe('<<=== ExcelOperations: (excel-helpers.test) ===>>', () => {
 
   before(function (done) {
     startListenPort(app, done);
+    makeDirSync([appRoot, 'test/data/tmp/ch-m52_acm']);
   });
 
   after(function (done) {
     stopListenPort(done);
+    removeFilesFromDirSync([appRoot, 'test/data/tmp/ch-m52_acm']);
   });
 
   it('#1: Get cells from xls file', async () => {
@@ -47,7 +54,25 @@ describe('<<=== ExcelOperations: (excel-helpers.test) ===>>', () => {
     }
   });
 
-  it('#2: Get cells from xlsx file', async () => {
+  it('#2: Write data to xls file', async () => {
+    // Read xls file
+    const cells = xlsxGetCellsFromFile([appRoot, xlsFile], 'Report1');
+    assert.ok(cells.length, 'Get cells from xls file');
+    for (let index = 0; index < cells.length; index++) {
+      const cell = cells[index];
+      if (isDebug_1 && loStartsWith(cell.address, 'A')) {
+        inspector('xls.cell:', loOmit(cell, ['xlsx', 'workbook', 'worksheet', 'cell']));
+      }      
+    }
+    // Write new data to xls file
+    const fileName = getFileName('DayHist01_14F120-', 'xls', true);
+    // const XLSX = cells[0].xlsx;
+    const workbook = cells[0].workbook;
+    
+    xlsxWriteToFile([appRoot, 'test/data/tmp/ch-m52_acm', fileName], workbook);
+  });
+
+  it('#3: Get cells from xlsx file', async () => {
     const cells = await exeljsGetCellsFromFile([appRoot, xlsxFile], 'Данi_СНВВ');
     // const cells = await exeljsGetCellsFromFile([appRoot, xlsxFile], 'Report1');
     assert.ok(cells.length, 'Get cells from xlsx file');
