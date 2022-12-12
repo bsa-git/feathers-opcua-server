f0ad790513e99b06e58922472c0817fa9482930e
Sergey Beskorovainy <bs261257@gmail.com>
2022-12-12 16:52:33 +0200

    Update

--- src/plugins/db-helpers/mssql-tedious.class.js
+++ src/plugins/db-helpers/mssql-tedious.class.js
@@ -5,12 +5,14 @@ const loOmit = require('lodash/omit');
 const { Connection, Request, TYPES } = require('tedious');
 
 const {
+  getter,
   inspector,
   isFunction,
   isObject
 } = require('../lib');
 
 const queryFuncs = require('./lib');
+const logger = require('../../logger');
 
 const debug = require('debug')('app:mssql-tedious.class');
 
@@ -108,7 +110,10 @@ class MssqlTedious {
       requestError: '',
       isConnected: false,
       isConnCanceled: false,
-      isConnReset: false
+      isConnReset: false,
+      isBeginTransaction: false,
+      isCommitTransaction: false,
+      isRollbackTransaction: false
     };
   }
 
@@ -363,6 +368,68 @@ class MssqlTedious {
     });
   }
 
+  /**
+   * @method beginTransaction
+   * @description SQL: Begin Transaction
+   */
+  beginTransaction() {
+    const self = this;
+    return new Promise((resolve, reject) => {
+      self.connection.beginTransaction((err) => {
+        if (err) {
+          // If error in begin transaction, roll back!
+          reject(err);
+        } else {
+          self.isBeginTransaction = true;
+          logger.info('BeginTransaction() done');
+          resolve('BeginTransaction() done');
+        }
+      });
+    });
+  }
+
+  /**
+   * @method commitTransaction
+   * @description SQL: Commit Transaction (if no errors)
+   */
+  commitTransaction() {
+    const self = this;
+    return new Promise((resolve, reject) => {
+      self.connection.commitTransaction((err) => {
+        if (err) {
+          logger.error(`Commit transaction err: ${err}`);
+          reject(err);
+        } else {
+          self.isBeginTransaction = false;
+          self.isCommitTransaction = true;
+          logger.info('CommitTransaction() done!');
+          resolve('CommitTransaction() done!');
+        }
+      });
+    });
+  }
+
+  /**
+   * @method rollbackTransaction
+   * @description SQL: Rolling Back Transaction - due to errors during transaction process.
+   */
+  rollbackTransaction(err) {
+    const self = this;
+    return new Promise((resolve, reject) => {
+      logger.error(`Transaction err: "${err}"`);
+      self.connection.rollbackTransaction((err) => {
+        if (err) {
+          logger.error(`Transaction rollback error: ${err}`);
+          reject(err);
+        } else {
+          self.isBeginTransaction = false;
+          self.isRollbackTransaction = true;
+          logger.info('RollbackTransaction() done!');
+          resolve('RollbackTransaction() done!');
+        }
+      });
+    });
+  }
 
   /**
    * @method query
@@ -563,15 +630,15 @@ class MssqlTedious {
         // The server has issued an information message.
         if (value.enable) self.connection.on('infoMessage', function (info) {
           /**
-                          info - An object with these properties:
-                            number - Error number
-                            state - The error state, used as a modifier to the error number.
-                            class - The class (severity) of the error. A class of less than 10 indicates an informational message.
-                            message - The message text.
-                            procName - The stored procedure name (if a stored procedure generated the message).
-                            lineNumber - The line number in the SQL batch or stored procedure that caused the error. 
-                                         Line numbers begin at 1; therefore, if the line number is not applicable to the message, the value of LineNumber will be 0. 
-                         */
+                              info - An object with these properties:
+                                number - Error number
+                                state - The error state, used as a modifier to the error number.
+                                class - The class (severity) of the error. A class of less than 10 indicates an informational message.
+                                message - The message text.
+                                procName - The stored procedure name (if a stored procedure generated the message).
+                                lineNumber - The line number in the SQL batch or stored procedure that caused the error. 
+                                             Line numbers begin at 1; therefore, if the line number is not applicable to the message, the value of LineNumber will be 0. 
+                             */
           value.cb ? value.cb(info) : self.onInfoMessageForConn(info);
         });
         break;
@@ -629,15 +696,15 @@ class MssqlTedious {
         // This event may be emited multiple times when more than one recordset is produced by the statement.
         if (value.enable) request.on('columnMetadata', function (columns) {
           /**
-                          An array like object, where the columns can be accessed either by index or name. 
-                          Columns with a name that is an integer are not accessible by name, as it would be interpreted as an array index.
-                          Each column has these properties.
-                            colName - The column's name.
-                            type.name - The column's type, such as VarChar, Int or Binary.
-                            precision - The precision. Only applicable to numeric and decimal.
-                            scale - The scale. Only applicable to numeric, decimal, time, datetime2 and datetimeoffset.
-                            dataLength - The length, for char, varchar, nvarchar and varbinary. 
-                         */
+                              An array like object, where the columns can be accessed either by index or name. 
+                              Columns with a name that is an integer are not accessible by name, as it would be interpreted as an array index.
+                              Each column has these properties.
+                                colName - The column's name.
+                                type.name - The column's type, such as VarChar, Int or Binary.
+                                precision - The precision. Only applicable to numeric and decimal.
+                                scale - The scale. Only applicable to numeric, decimal, time, datetime2 and datetimeoffset.
+                                dataLength - The length, for char, varchar, nvarchar and varbinary. 
+                             */
           value.cb ? value.cb(columns) : self.onColumnMetadataForRequest(columns);
         });
         break;
@@ -661,22 +728,22 @@ class MssqlTedious {
         break;
       case 'done':
         /**
-                       All rows from a result set have been provided (through row events). 
-                       This token is used to indicate the completion of a SQL statement. 
-                       As multiple SQL statements can be sent to the server in a single SQL batch, multiple done events can be generated. 
-                       An done event is emited for each SQL statement in the SQL batch except variable declarations. 
-                       For execution of SQL statements within stored procedures, doneProc and doneInProc events are used in place of done events.
-            
-                       If you are using execSql then SQL server may treat the multiple calls with the same query as a stored procedure. 
-                       When this occurs, the doneProc or doneInProc events may be emitted instead. 
-                       You must handle both events to ensure complete coverage. 
-                       */
+                           All rows from a result set have been provided (through row events). 
+                           This token is used to indicate the completion of a SQL statement. 
+                           As multiple SQL statements can be sent to the server in a single SQL batch, multiple done events can be generated. 
+                           An done event is emited for each SQL statement in the SQL batch except variable declarations. 
+                           For execution of SQL statements within stored procedures, doneProc and doneInProc events are used in place of done events.
+                
+                           If you are using execSql then SQL server may treat the multiple calls with the same query as a stored procedure. 
+                           When this occurs, the doneProc or doneInProc events may be emitted instead. 
+                           You must handle both events to ensure complete coverage. 
+                           */
         if (value.enable) request.on('done', function (rowCount, more, rows) {
           /**
-                          rowCount - The number of result rows. May be undefined if not available.
-                          more - If there are more results to come (probably because multiple statements are being executed), then true.
-                          rows - Rows as a result of executing the SQL statement. Will only be avaiable if Connection's config.options.rowCollectionOnDone is true. 
-                         */
+                              rowCount - The number of result rows. May be undefined if not available.
+                              more - If there are more results to come (probably because multiple statements are being executed), then true.
+                              rows - Rows as a result of executing the SQL statement. Will only be avaiable if Connection's config.options.rowCollectionOnDone is true. 
+                             */
           value.cb ? value.cb(rowCount, more, rows) : self.onDoneForRequest(rowCount, more, rows);
         });
         break;
@@ -684,10 +751,10 @@ class MssqlTedious {
         // A value for an output parameter (that was added to the request with addOutputParameter(...)).
         if (value.enable) request.on('returnValue', function (parameterName, value, metadata) {
           /**
-                          parameterName - The parameter name. (Does not start with '@'.)
-                          value - The parameter's output value.
-                          metadata - The same data that is exposed in the columnMetadata event. 
-                         */
+                              parameterName - The parameter name. (Does not start with '@'.)
+                              value - The parameter's output value.
+                              metadata - The same data that is exposed in the columnMetadata event. 
+                             */
           value.cb ? value.cb(parameterName, value, metadata) : self.onReturnValueForRequest(parameterName, value, metadata);
         });
         break;
@@ -695,8 +762,8 @@ class MssqlTedious {
         // This event gives the columns by which data is ordered, if ORDER BY clause is executed in SQL Server.
         if (value.enable) request.on('order', function (orderColumns) {
           /**
-                          orderColumns - An array of column numbers in the result set by which data is ordered. 
-                         */
+                              orderColumns - An array of column numbers in the result set by which data is ordered. 
+                             */
           value.cb ? value.cb(orderColumns) : self.onOrderForRequest(orderColumns);
         });
         break;
--- test/debug/mssql-tedious.test.js
+++ test/debug/mssql-tedious.test.js
@@ -2,10 +2,12 @@
 const assert = require('assert');
 const app = require('../../src/app');
 const papa = require('papaparse');
+const moment = require('moment');
 
 const {
   appRoot,
   inspector,
+  getRandomValue,
   startListenPort,
   stopListenPort,
   MssqlTedious,
@@ -67,18 +69,30 @@ describe('<<=== MSSQL-Tedious Test (mssql-tedious.test.js) ===>>', () => {
     stopListenPort(done);
   });
 
-  it('#1: Delete rows from table', async () => {
-    const db = new MssqlTedious(mssqlEnvName);
-    await db.connect();
-    await db.query([], 'DELETE FROM dbo.tblMessages');
-    const rows = await db.query([], 'SELECT * FROM tblMessages');
-    if(isDebug) inspector('Delete result:', { rows });
-    await db.disconnect();
-
-    assert.ok(!rows.length, 'Delete rows from table');
+  it('#1: Delete rows from table with transactions', async () => {
+    let db;
+    try {
+      db = new MssqlTedious(mssqlEnvName);
+      await db.connect();
+      
+      // Begin transaction      
+      await db.beginTransaction();
+      await db.query([], 'DELETE FROM dbo.tblMessages');
+      const rows = await db.query([], 'SELECT * FROM tblMessages');
+      if (isDebug && rows) inspector('Delete result:', { rows });
+      
+      // Commit transaction      
+      await db.commitTransaction();
+      await db.disconnect();
+      assert.ok(!rows.length, 'Delete rows from table with transactions');
+    } catch (error) {
+      // Rollback transaction      
+      await db.rollbackTransaction(error.message);
+      assert.ok(false, 'Delete rows from table with transactions');
+    }
   });
 
-  
+
   it('#2: Insert "CH_M51" rows to table', async () => {
     let sql = '', rows;
     //---------------------------------------------
@@ -89,7 +103,7 @@ describe('<<=== MSSQL-Tedious Test (mssql-tedious.test.js) ===>>', () => {
     let params = [];
     sql = 'INSERT INTO dbo.tblMessages VALUES (@value, @type, @text)';
     const jsonData = getData('data-CH_M51.csv');
-    if(isDebug && jsonData) inspector('getData:', jsonData);
+    if (isDebug && jsonData) inspector('getData:', jsonData);
     db.buildParams(params, 'value', TYPES.Char, 'CH_M51::ValueFromFile');
     db.buildParams(params, 'type', TYPES.Char, 'tag');
     db.buildParams(params, 'text', TYPES.Char, JSON.stringify(jsonData));
@@ -103,7 +117,7 @@ describe('<<=== MSSQL-Tedious Test (mssql-tedious.test.js) ===>>', () => {
     db.buildParams(params, 'value', TYPES.Char, 'CH_M51::ValueFromFile');
     db.buildParams(params, 'text', TYPES.Char, null, true);
     rows = await db.query(params, sql);
-    if(isDebug && rows) inspector('Request result:', { sql, rows });
+    if (isDebug && rows) inspector('Request result:', { sql, rows });
     await db.disconnect();
 
     const jsonRows = JSON.parse(rows[0]['text']);
@@ -120,7 +134,7 @@ describe('<<=== MSSQL-Tedious Test (mssql-tedious.test.js) ===>>', () => {
     let params = [];
     sql = 'INSERT INTO dbo.tblMessages VALUES (@value, @type, @text)';
     const jsonText = getData('data-CH_M52.csv');
-    if(isDebug) inspector('getData:', jsonText);
+    if (isDebug) inspector('getData:', jsonText);
     db.buildParams(params, 'value', TYPES.Char, 'CH_M52::ValueFromFile');
     db.buildParams(params, 'type', TYPES.Char, 'tag');
     db.buildParams(params, 'text', TYPES.Char, JSON.stringify(jsonText));
@@ -133,7 +147,7 @@ describe('<<=== MSSQL-Tedious Test (mssql-tedious.test.js) ===>>', () => {
     db.buildParams(params, 'type', TYPES.Char, 'tag');
     db.buildParams(params, 'value', TYPES.Char, 'CH_M52::ValueFromFile');
     rows = await db.query(params, sql);
-    if(isDebug) inspector('Request result:', { sql, rows });
+    if (isDebug) inspector('Request result:', { sql, rows });
     await db.disconnect();
 
     assert.deepStrictEqual(JSON.parse(rows[0]['Text']), jsonText, 'Insert rows to table');
@@ -149,7 +163,7 @@ describe('<<=== MSSQL-Tedious Test (mssql-tedious.test.js) ===>>', () => {
     let params = [];
     sql = 'UPDATE dbo.tblMessages SET Text = @text WHERE Type = @type AND Value = @value';
     const jsonText = getData('data-CH_M51.csv');
-    if(isDebug) inspector('getData:', jsonText);
+    if (isDebug) inspector('getData:', jsonText);
     db.buildParams(params, 'value', TYPES.Char, 'CH_M51::ValueFromFile');
     db.buildParams(params, 'type', TYPES.Char, 'tag');
     db.buildParams(params, 'text', TYPES.Char, JSON.stringify(jsonText));
@@ -161,14 +175,14 @@ describe('<<=== MSSQL-Tedious Test (mssql-tedious.test.js) ===>>', () => {
     db.buildParams(params, 'type', TYPES.Char, 'tag');
     db.buildParams(params, 'value', TYPES.Char, 'CH_M51::ValueFromFile');
     rows = await db.query(params, sql);
-    if(isDebug) inspector('Request result:', { sql, rows });
-    
+    if (isDebug) inspector('Request result:', { sql, rows });
+
     await db.disconnect();
 
     assert.deepStrictEqual(JSON.parse(rows[0]['Text']), jsonText, 'Update value from table');
   });
 
-  
+
   it('#5: Execute Stored Procedure "dbo.MessagesSummary"', async () => {
     let sql = '';
     //---------------------------------------------
@@ -183,14 +197,14 @@ describe('<<=== MSSQL-Tedious Test (mssql-tedious.test.js) ===>>', () => {
     db.buildParams(params, 'text', TYPES.Char, null, true);
     // db.buildParams(params, '@text', TYPES.Char, '');
     const rows = await db.proc(params, sql);
-    if(isDebug) inspector('Stored procedure result:', { sql, rows });
+    if (isDebug) inspector('Stored procedure result:', { sql, rows });
     // inspector('Stored procedure result:', { sql, rows });
-    
+
     await db.disconnect();
 
     assert.ok(rows[0].text, 'Execute Stored Procedure "dbo.MessagesSummary"');
   });
-  
+
   it('#6: Select values for "webM51" from SnapShot table', async () => {
     let sql = '', rows;
     //---------------------------------------------
@@ -206,10 +220,10 @@ describe('<<=== MSSQL-Tedious Test (mssql-tedious.test.js) ===>>', () => {
     WHERE sh.ScanerName = @scanerName AND tInfo.OnOff = 1
     `;
     db.buildParams(params, 'scanerName', TYPES.Char, 'webM51');
-    
+
     rows = await db.query(params, sql);
-    if(isDebug) inspector('Request result:', { sql, rows });
-    
+    if (isDebug) inspector('Request result:', { sql, rows });
+
     await db.disconnect();
 
     assert.ok(rows.length, 'Select values for "webM51" from SnapShot table');
@@ -230,45 +244,94 @@ describe('<<=== MSSQL-Tedious Test (mssql-tedious.test.js) ===>>', () => {
     WHERE sh.ScanerName = @scanerName AND tInfo.OnOff = 1
     `;
     db.buildParams(params, 'scanerName', TYPES.Char, 'opcUPG2');
-        
+
     rows = await db.query(params, sql);
-    if(isDebug && rows) inspector('Request result:', { sql, rows });
-    
+    if (isDebug && rows) inspector('Request result:', { sql, rows });
+
     await db.disconnect();
 
     assert.ok(rows.length, 'Select values for "opcUPG2" from SnapShot table');
   });
 
-  it('#8: Select values for "opcUA(A5)" from TagsInfo table', async () => {
-    let sql = '', rows;
-    const scanerName = 'opcUA(A5)'; 
+  it('#8: Add values to SnapShot table for "opcUA(A5)" and "XozUchet"', async () => {
+    let sql = '', sql2 = '', rows, rowSnapShot = {}, rowsSnapShot = [];
+    const scanerName = 'opcUA(A5)';
     const tagGroup = 'XozUchet';
     //---------------------------------------------
     const db = new MssqlTedious(mssqlEnvName);
     await db.connect();
 
     // Select rows from SnapShot table
-    const params = [];
+    let params = [];
     sql = `
-    SELECT tInfo.TagName, tInfo.ID 
+    SELECT tInfo.ID, tInfo.ScaleMin, tInfo.ScaleMax
     FROM dbConfig.dbo.TagsInfo AS tInfo
     WHERE tInfo.ScanerName = @scanerName AND tInfo.TagGroup = @tagGroup
     ORDER BY ID
     `;
 
-    sql = `
-    SELECT sh.Value, sh.Time, sh.TagId, tInfo.TagName, tInfo.ScanerName, tInfo.TagGroup, tInfo.KIPname
-    FROM dbMonitor.dbo.SnapShot AS sh
-    JOIN dbConfig.dbo.TagsInfo AS tInfo ON sh.TagID = tInfo.ID
-    WHERE (sh.ScanerName = @scanerName) AND (tInfo.OnOff = 1) AND (tInfo.TagGroup = @tagGroup)
-    `;
-        
     db.buildParams(params, 'scanerName', TYPES.Char, scanerName);
     db.buildParams(params, 'tagGroup', TYPES.Char, tagGroup);
-    
+
     rows = await db.query(params, sql);
-    if(true && rows) inspector('Request result.rows:', rows);
-    
+    if (isDebug && rows) inspector('Get tags from TagsInfo.rows:', rows);
+
+    for (let index = 0; index < rows.length; index++) {
+      const row = rows[index];
+      const dt = moment();
+      let val = (row.ScaleMax - row.ScaleMin) / 2;
+      val = getValue(val);
+
+      rowSnapShot['TagID'] = row.ID;
+      rowSnapShot['ScanerName'] = scanerName;
+      rowSnapShot['Time'] = dt;
+      rowSnapShot['dtYear'] = dt.year();
+      rowSnapShot['dtDofY'] = dt.dayOfYear();
+      rowSnapShot['dtTotalS'] = (dt.hours() * 60) + dt.seconds();
+      rowSnapShot['Value'] = val;
+    }
+    if (isDebug && rowsSnapShot) inspector('Get tags from TagsInfo.rowsSnapShot:', rowSnapShot);
+
+    // Remove rows from dbBSA.dbo.SnapShotTest
+    await db.query([], 'DELETE FROM dbBSA.dbo.SnapShotTest');
+
+    // Insert row to dbBSA.dbo.SnapShotTest
+    params = [];
+    sql = 'INSERT INTO dbBSA.dbo.SnapShotTest VALUES (@tagID, @scanerName, @time, @dtYear, @dtDofY, @dtTotalS, @value)';
+
+    db.buildParams(params, 'tagID', TYPES.Int, rowSnapShot['TagID']);
+    db.buildParams(params, 'scanerName', TYPES.Char, rowSnapShot['ScanerName']);
+    db.buildParams(params, 'time', TYPES.DateTime, rowSnapShot['Time']);
+    db.buildParams(params, 'dtYear', TYPES.SmallInt, rowSnapShot['dtYear']);
+    db.buildParams(params, 'dtDofY', TYPES.SmallInt, rowSnapShot['dtDofY']);
+    db.buildParams(params, 'dtTotalS', TYPES.Int, rowSnapShot['dtTotalS']);
+    db.buildParams(params, 'value', TYPES.Real, rowSnapShot['Value']);
+    await db.query(params, sql);
+
+    sql2 = `
+    SELECT sh.Value, sh.Time, sh.TagId, tInfo.TagName, tInfo.ScanerName, tInfo.TagGroup, tInfo.KIPname
+    FROM dbBSA.dbo.SnapShotTest AS sh
+    JOIN dbConfig.dbo.TagsInfo AS tInfo ON sh.TagID = tInfo.ID
+    WHERE (sh.ScanerName = @scanerName) AND (tInfo.TagGroup = @tagGroup) AND (tInfo.OnOff = 1)
+    `;
+
+    sql2 = `
+    SELECT sh.TagId, sh.ScanerName, sh.Time, sh.dtYear, sh.dtDofY, sh.dtTotalS, sh.Value
+    FROM dbBSA.dbo.SnapShotTest AS sh
+    JOIN dbConfig.dbo.TagsInfo AS tInfo ON sh.TagID = tInfo.ID
+    WHERE (sh.ScanerName = @scanerName) AND (tInfo.TagGroup = @tagGroup)
+    `;
+
+    params = [];
+    db.buildParams(params, 'scanerName', TYPES.Char, scanerName);
+    db.buildParams(params, 'tagGroup', TYPES.Char, tagGroup);
+
+    rows = await db.query(params, sql2);
+    rows[0]['Time'] = moment(rows[0]['Time']).format();
+    if (true && rows) inspector('Get values from SnapShot.rows:', rows);
+
+
+
     await db.disconnect();
 
     assert.ok(rows, 'Select values for "opcUA(A5)" from TagsInfo table');
