27078000e8fe1ad07766e2e99c9b532b301ac6e6
Sergey Beskorovainy <bs261257@gmail.com>
2022-12-28 10:55:54 +0200

    Update

--- scripts/cli/install-windows.service.js
+++ scripts/cli/install-windows.service.js
@@ -2,11 +2,10 @@ const { cwd } = require('process');
 const appRoot = cwd();
 const { join } = require('path');
 const filePath = join(appRoot, 'src\\index.js');
-// console.log('filePath:', filePath);
-var Service = require('node-windows').Service;
+const Service = require('node-windows').Service;
 
 // Create a new service object
-var svc = new Service({
+const svc = new Service({
   name: 'Feathers opcua server',
   description: 'Feathers opcua server application as Windows Service',
   script: filePath,
