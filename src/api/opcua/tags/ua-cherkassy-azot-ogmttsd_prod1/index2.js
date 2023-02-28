357b596f383f57ccd17c6f32cffdf9ac7db7ea5a
Sergey Beskorovainy <bs261257@gmail.com>
2023-02-28 14:38:57 +0200

    Update

--- src/api/opcua/tags/ua-cherkassy-azot-ogmttsd_prod1/index2.js
+++ src/api/opcua/tags/ua-cherkassy-azot-ogmttsd_prod1/index2.js
@@ -1,7 +1,7 @@
 const loMergeWith = require('lodash/mergeWith');
 const CH_A5_XozUchet_Options = require('./CH-A5_XozUchet_Options.json');
 const CH_A5_XozUchet_Daily_Options = require('./CH-A5_XozUchet_Daily_Options.json');
-const CH_A5_Mnemo1_Options = require('./CH-A5_Mnemo1_Options.json');
+const CH_A5_TexnoMnemo1_Options = require('./CH-A5_TexnoMnemo1_Options.json');
 
 function customizer(objValue, srcValue) {
   if (Array.isArray(objValue)) {
@@ -9,7 +9,7 @@ function customizer(objValue, srcValue) {
   }
 }
 
-let mergeResult = loMergeWith(CH_A5_XozUchet_Options, CH_A5_Mnemo1_Options, customizer);
+let mergeResult = loMergeWith(CH_A5_XozUchet_Options, CH_A5_TexnoMnemo1_Options, customizer);
 mergeResult = loMergeWith(mergeResult, CH_A5_XozUchet_Daily_Options, customizer);
 // mergeResult = loMergeWith(mergeResult, CH_A5_Mnemo1_Options, customizer);
 
